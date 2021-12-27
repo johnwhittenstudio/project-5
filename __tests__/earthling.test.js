129362d3ee8b278f9297af3c79380eb7b36574f0
John Whitten <johnwhitten.studio@gmail.com>
2021-12-27 09:59:08 -0800

    add test 2, calculate mercury age

--- __tests__/earthling.test.js
+++ __tests__/earthling.test.js
@@ -8,14 +8,13 @@ describe('Earthling', () => {
   });
 
   test("should correctly create a earthling object with the person's age, lifespan, and years remaining ", () => {
-    // const earthling = new Earthling(37,79,42);
     expect(earthling.age).toEqual(37);
     expect(earthling.lifespan).toEqual(80);
-    expect(earthling.lifeLeft).toEqual(42);
+    expect(earthling.lifeLeft).toEqual(43);
   });
 
-  test("should calculate the earthling age into age on Maercury ", () => {
-    let ageOnMercury =  Math.round(earthling.age / .24)
+  test("should calculate the earthling age into age on Mercury ", () => {
+    expect(earthling.ageOnMercury()).toEqual(154);
   });
 
 });
