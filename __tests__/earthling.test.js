74eee86cb286661bdb12f861fdb67c7c191b58b1
John Whitten <johnwhitten.studio@gmail.com>
2021-12-27 09:32:21 -0800

    describe ('Earthling' create object with age, lifespan, and life left

--- __tests__/earthling.test.js
+++ __tests__/earthling.test.js
@@ -1,2 +1,13 @@
 import Earthling from './../src/js/earthling.js';
 
+describe('Earthling', () => {
+  let earthling;
+
+
+  test("should correctly create a earthling object with the person's age, lifespan, and years remaining ", () => {
+    expect(earthling.age).toEqual(37);
+    expect(earthling.lifespan).toEqual(79);
+    expect(earthling.lifeLeft).toEqual(42);
+  });
+
+});
