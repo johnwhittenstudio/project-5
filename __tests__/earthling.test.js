5747eb9f482fc65a9d694fc7c646118cbad97f84
John Whitten <johnwhitten.studio@gmail.com>
2021-12-27 09:34:36 -0800

    test 1: create earthling object passed

--- __tests__/earthling.test.js
+++ __tests__/earthling.test.js
@@ -5,6 +5,7 @@ describe('Earthling', () => {
 
 
   test("should correctly create a earthling object with the person's age, lifespan, and years remaining ", () => {
+    const earthling = new Earthling(37,79,42);
     expect(earthling.age).toEqual(37);
     expect(earthling.lifespan).toEqual(79);
     expect(earthling.lifeLeft).toEqual(42);
